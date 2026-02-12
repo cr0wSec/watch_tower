6dc892e499a633457307e22a501bc37e9501a840
John Doe <204260440+cr0wSec@users.noreply.github.com>
2026-02-12 13:28:11 +0100

    Refactor main and lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,51 @@ pub mod templates;
 pub mod utils;
 
 use axum::{Router, routing::get};
+use axum::routing::post;
+use deadpool_diesel::postgres::Pool;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use controllers::ui::views::get_index;
 use tower_http::trace::TraceLayer;
+use crate::controllers::api::auth::{post_login, post_register};
+use crate::controllers::ui::views::{get_login, get_register};
 
-pub fn app() -> Router {
+pub type DbPool = Pool;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("infra/migrations");
+
+pub fn create_pool(database_url: &str) -> DbPool {
+    let manager = deadpool_diesel::postgres::Manager::new(
+        database_url.to_string(),
+        deadpool_diesel::Runtime::Tokio1,
+    );
+    Pool::builder(manager).build().unwrap()
+}
+
+pub async fn run_migrations(pool: &DbPool) {
+    let conn = pool.get().await.unwrap();
+    conn.interact(|conn| conn.run_pending_migrations(MIGRATIONS).map(|_| ()))
+        .await
+        .unwrap()
+        .unwrap();
+}
+
+pub fn public_routes() -> Router {
     Router::new()
         .route("/", get(get_index))
+        .route("/login", get(get_login))
+        .route("/register", get(get_register))
+}
+
+pub fn auth_routes(pool: DbPool) -> Router {
+    Router::new()
+        .route("/auth/login", post(post_login))
+        .route("/auth/register", post(post_register))
+        .with_state(pool)
+}
+
+pub fn app(pool: DbPool) -> Router {
+
+    public_routes()
+        .nest("/api", auth_routes(pool))
         .layer(TraceLayer::new_for_http())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::net::SocketAddr;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-use watch_tower::app;
+use watch_tower::{app, create_pool, run_migrations};
+
 
 #[tokio::main]
 async fn main() {
@@ -14,10 +15,16 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let _ = dotenvy::dotenv(); // only needed in dev
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let pool = create_pool(&db_url);
+    run_migrations(&pool).await;
+
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
     tracing::debug!("listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app())
+    axum::serve(listener, app(pool))
         .await
         .expect("failed to start the server");
 }
